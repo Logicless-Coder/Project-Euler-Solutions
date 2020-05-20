46db7697040a0f3a715c091cf21bbbc9bd10b470
Logicless-Coder <59042344+Logicless-Coder@users.noreply.github.com>
2020-05-20 17:45:05 +0530

    Create Problem 21.cpp

---
+++ Problems 21-30/Problem 21.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <chrono>
+
+using namespace std;
+
+int d(int x) {
+    int sum = 0;
+    for (int i=1; i<x; i++) {
+        if (x%i == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    auto start = chrono::high_resolution_clock::now();
+
+    int n = 10000;
+    long long ans = 0;
+    vector<int> seen;
+    for (int i=1; i<n; i++) {
+        int j = d(i);
+        auto it = find(seen.begin(), seen.end(), i);
+        if (d(j) == i && i != j) {
+            if (it == seen.end()) {
+                ans += i;
+                seen.emplace_back(i);
+            }
+            cout << i << " " << j << endl;
+        }
+    }
+
+    cout << ans << endl;
+
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout << duration.count() << "ms" << endl;
+    return 0;
+}
